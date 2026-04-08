0240a03bc928dd5b86bad783b3ca948523cf34d5
pipickeke <ycltony7398@163.com>
2026-04-08 14:46:39 +0800

    #2026-04-08 滑动窗口

---
+++ src/com.pipickeke/year2026/month04/week1/3652. 按策略买卖股票的最佳时机.py
@@ -0,0 +1,57 @@
+"""
+
+3652. 按策略买卖股票的最佳时机
+
+给你两个整数数组 prices 和 strategy，其中：
+
+prices[i] 表示第 i 天某股票的价格。
+strategy[i] 表示第 i 天的交易策略，其中：
+-1 表示买入一单位股票。
+0 表示持有股票。
+1 表示卖出一单位股票。
+同时给你一个 偶数 整数 k，你可以对 strategy 进行 最多一次 修改。一次修改包括：
+
+选择 strategy 中恰好 k 个 连续 元素。
+将前 k / 2 个元素设为 0（持有）。
+将后 k / 2 个元素设为 1（卖出）。
+利润 定义为所有天数中 strategy[i] * prices[i] 的 总和 。
+
+返回你可以获得的 最大 可能利润。
+
+注意： 没有预算或股票持有数量的限制，因此所有买入和卖出操作均可行，无需考虑过去的操作。
+
+"""
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices:List[int], strategy:List[int], k:int)->int:
+        total = s = max_s = 0
+        for i, (p, st) in enumerate(zip(prices, strategy)):
+            total += p * st
+
+            # 下标i的元素进入右半，策略从st变成了1
+            s += p * (1 - st)
+
+            if i < k-1: # 尚未形成第一个窗口
+                #在下一轮循环中，下标为i-k/2+1的元素从右半移到左半
+                #交易策略从1变成0
+                if i >= k//2 - 1:
+                    s -= prices[i - k//2 + 1]
+                continue
+
+            max_s = max(max_s, s)
+
+            s -= prices[i - k//2 + 1] - prices[i-k+1] * strategy[i-k+1]
+
+        return total + max_s
+
+
+
+
+
+
+
+
+
+
